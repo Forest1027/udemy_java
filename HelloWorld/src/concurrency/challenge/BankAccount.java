c242627829762201a35402b20ace179c09541370
Forest Lettuce <azureforestryan@gmail.com>
2020-05-25 08:18:29 +0100

    concurrency challenges

--- HelloWorld/src/concurrency/challenge/BankAccount.java
+++ HelloWorld/src/concurrency/challenge/BankAccount.java
@@ -1,4 +1,80 @@
 package concurrency.challenge;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
+    private volatile double balance;
+    private String accountNumber;
+    private ReentrantLock lock;
+
+    public BankAccount(double initialBalance, String accountNumber) {
+        this.balance = initialBalance;
+        this.accountNumber = accountNumber;
+        lock = new ReentrantLock();
+    }
+
+    public void deposit(double amount) {
+        while (true) {
+            boolean status = false;
+            try {
+                if (lock.tryLock(10, TimeUnit.MILLISECONDS)) {
+                    try {
+                        balance += amount;
+                        status = true;
+                        System.out.println(Thread.currentThread().getName() + " deposited " + amount + ", balance is " + this.getBalance());
+                        break;
+                    } finally {
+                        lock.unlock();
+                    }
+                } else {
+                    System.out.println(Thread.currentThread().getName() + " could not get the lock.");
+                }
+            } catch (InterruptedException e) {
+
+            }
+            System.out.println(Thread.currentThread().getName() + " Transaction status:" + status);
+        }
+
+    }
+
+    public void withdraw(double amount) {
+        while (true) {
+            boolean status = false;
+            try {
+                if (lock.tryLock(10, TimeUnit.MILLISECONDS)) {
+                    try {
+                        balance -= amount;
+                        status = true;
+                        System.out.println(Thread.currentThread().getName() + " withdrew " + amount + ", balance is " + this.getBalance());
+                        break;
+                    } finally {
+                        lock.unlock();
+                    }
+                } else {
+                    System.out.println(Thread.currentThread().getName() + " could not get the lock.");
+                }
+            } catch (InterruptedException e) {
+
+            }
+            System.out.println(Thread.currentThread().getName() + " Transaction status:" + status);
+        }
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 }
--- HelloWorld/src/concurrency/challenge/Challenge.java
+++ HelloWorld/src/concurrency/challenge/Challenge.java
@@ -1,14 +1,27 @@
 package concurrency.challenge;
 
-public class Challenge1 {
+public class Challenge {
     /**
+     * Challenge 1:
      * Create and start threads
      * We could have two people using a joint bank account at the same time.
      * Create and start two threads that use the same BankAccount instance and an initial balance of $1000.00.
      * One will deposit $300.00 into the bank account, and then withdraw $50.00.
      * The other will deposit $203.75 and then withdraw $100.00
-     *
+     * <p>
      * solution: deposit and withdraw methods are decorated by synchronized keyword
+     *
+     * Challenge 4:
+     * use ReentrantLock
+     *
+     * Challenge 5:
+     * use trylock with a timeout value
+     * Instead of using lock(), use tryLock() with a timeout value of 1 second.
+     * If the waiting period times out, print the message, "Could not get the lock" to the console
+     *
+     * Challenge 6:
+     * update the code so that the status variable is thread safe
+     * use whatever method you like: the synchronized keyword or the lock object
      *
      * @param args
      */
